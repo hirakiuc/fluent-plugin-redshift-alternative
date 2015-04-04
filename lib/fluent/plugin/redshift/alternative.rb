9452c77c1e6f8211734ac2698606bc51a3cd6f72
hirakiuc <hirakiuc@gmail.com>
2015-04-04 09:47:44 +0900

    initial import

---
+++ lib/fluent/plugin/redshift/alternative.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/redshift/alternative/version"
+
+module Fluent
+  module Plugin
+    module Redshift
+      module Alternative
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/redshift/alternative/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Redshift
+      module Alternative
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
