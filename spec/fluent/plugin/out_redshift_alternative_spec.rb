7b6bd8ee868b07331453f147632a1e222b435f27
hirakiuc <hirakiuc@gmail.com>
2015-04-05 17:36:49 +0900

    fix spec_helper. add empty spec file

---
+++ spec/fluent/plugin/out_redshift_alternative_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+RSpec.describe Fluent::RedshiftAlternativeOutput do
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,49 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'rspec'
+require 'webmock/rspec'
+
+require 'pry'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'fluent/plugin/redshift/alternative'
+require 'fluent/test'
+require 'fluent/plugin/out_redshift_alternative'
+
+# Disable Test::Unit
+module Test::Unit::RunCount; def run(*); end; end
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    add_filter 'spec'
+    add_filter 'pkg'
+    add_filter '.bundle'
+  end
+
+  Coveralls.wear!
+end
+
+if ENV['CODECLIMATE_REPORT']
+  WebMock.disable_net_connect!(allow: 'codeclimate.com')
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
+path = Pathname.new(Dir.pwd)
+Dir[path.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.before(:all) do
+    Fluent::Test.setup
+  end
+
+  config.order = 'random'
+end
